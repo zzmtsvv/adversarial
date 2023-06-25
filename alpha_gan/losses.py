67f4454107b37e8d3b631cb0bd97dafa96944a10
Alexey Zemtsov <m2209856@edu.misis.ru>
2023-06-26 00:22:24 +0300

    Update losses.py

--- alpha_gan/losses.py
+++ alpha_gan/losses.py
@@ -3,49 +3,6 @@ from torch import nn
 from torch.nn.modules.loss import _Loss
 
 
-class CodeReconstructionLoss(_Loss):
-    '''
-        the input is considered to be a hidden representation of the encoder
-        and the desired latent distribution.
-    '''
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.loss = nn.MSELoss()
-    
-    def forward(self, encoded: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        return self.loss(encoded, target)
-
-
-class DiscriminatorLoss(_Loss):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.loss = nn.MSELoss()
-    
-    def forward(self, real_scores: torch.Tensor, generated_scores: torch.Tensor) -> torch.Tensor:
-        labels_true = torch.ones_like(real_scores)
-        labels_fake = torch.zeros_like(generated_scores)
-
-        loss = self.loss(real_scores, labels_true) + self.loss(generated_scores, labels_fake)
-
-        return loss
-
-
-class GeneratorLoss(_Loss):
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.loss = nn.MSELoss()
-    
-    def forward(self, generator_score: torch.Tensor) -> torch.Tensor:
-        assert len(generator_score.shape) == 2  # [batch_size, 1]
-
-        target = torch.ones_like(generator_score)
-
-        return self.loss(generator_score, target)
-
-
 class ReconstructionLoss(_Loss):
     '''
         the input is considered to be real images and reconstructed ones by generator
@@ -56,6 +13,76 @@ class ReconstructionLoss(_Loss):
 
         self.loss = nn.L1Loss()
     
-    def forward(self, reconstructed: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+    def forward(self,
+                reconstructed: torch.Tensor,
+                target: torch.Tensor) -> torch.Tensor:
 
         return self.loss(reconstructed, target)
+
+
+class EncoderLoss(_Loss):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.loss = nn.BCEWithLogitsLoss()
+    
+    def forward(self, encoder_scores: torch.Tensor) -> torch.Tensor:
+        labels_true = torch.ones_like(encoder_scores)
+        labels_fake = torch.zeros_like(encoder_scores)
+
+        loss = self.loss(encoder_scores, labels_true) - self.loss(encoder_scores, labels_fake)
+        return loss
+
+
+class GeneratorLoss(_Loss):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.loss = nn.BCEWithLogitsLoss()
+    
+    def forward(self,
+                reconstructed_scores: torch.Tensor,
+                generator_scores: torch.Tensor) -> torch.Tensor:
+        assert len(generator_scores.shape) == len(reconstructed_scores.shape) == 2  # [batch_size, 1]
+
+        labels_true = torch.ones_like(reconstructed_scores)
+        labels_fake = torch.zeros_like(generator_scores)
+
+        recon_loss = self.loss(reconstructed_scores, labels_true) - self.loss(reconstructed_scores, labels_fake)
+        gener_loss = self.loss(generator_scores, labels_true) - self.loss(generator_scores, labels_fake)
+        return recon_loss + gener_loss
+
+
+class DiscriminatorLoss(_Loss):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.loss = nn.BCEWithLogitsLoss()
+    
+    def forward(self,
+                real_scores: torch.Tensor,
+                reconstructed_scores: torch.Tensor,
+                generated_scores: torch.Tensor) -> torch.Tensor:
+        labels_true = torch.ones_like(real_scores)
+        labels_fake = torch.zeros_like(generated_scores)
+
+        loss = self.loss(real_scores, labels_true) + self.loss(generated_scores, labels_fake)
+        loss += self.loss(reconstructed_scores, labels_fake)
+
+        return loss
+
+
+class CodecriminatorLoss(_Loss):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.loss = nn.BCEWithLogitsLoss()
+    
+    def forward(self,
+                encoder_scores: torch.Tensor,
+                target_scores: torch.Tensor) -> torch.Tensor:
+        labels_true = torch.ones_like(encoder_scores)
+        labels_fake = torch.zeros_like(target_scores)
+
+        loss = self.loss(encoder_scores, labels_fake) + self.loss(target_scores, labels_true)
+        return loss
